b1154658039c19cbba049009f91a77584015190e
AlanSoto31 <soto.alan@hotmail.com>
2021-04-07 11:50:16 -0500

    Add table styling to the todos

--- src/doom.js
+++ src/doom.js
@@ -19,6 +19,61 @@ class UI {
     }
 
     static printTask(list) {
+        let todoBody = document.getElementById("todoBody");
+        todoBody.innerHTML = '';
+        list.forEach((task, index) => {
+          let taskCard = document.createElement("tr");
+          taskCard.innerHTML = 
+          `
+            <th scope="row">${index + 1}</th>
+            <td>${task.title}</td>
+            <td>${task.dueDate}</td>
+            <td><button class="delete-btn btn-danger">DONE</button></td>
+          `;
+          todoBody.appendChild(taskCard);
+        });
+      }
+
+      static printForm() {
+        const todos = document.querySelector("#todos");
+        let form = document.createElement("div");
+        form.classList.add("none");
+        form.setAttribute("id", "formCont");
+        form.add
+form.innerHTML = `
+
+<form action="/" class="d-flex flex-column w-25 mx-auto" id="todoForm">
+<label for="title" class="form-label">Title</label>
+<input type="text" id="title" class="form-control" required>
+
+<label for="description" class="form-label">Description</label>
+<input type="text" id="description" class="form-control" required>
+
+<label for="dueDate" class="form-label">Due Date</label>
+<input type="date" id="dueDate" class="form-control" required>
+
+<label for="priority" class="form-label">Priority</label>
+<select id="priority" name="priority" class="form-control" required>
+  <option value="low">low</option>
+  <option value="mid">Mid</option>
+  <option value="high">High</option>
+</select>
+
+<input type="submit" id="submit-btn" value="Create a new To Do" class="btn btn-primary">
+</form> 
+
+`;
+todos.appendChild(form);
+      }
+}
+
+export default UI;
+
+
+
+
+/*
+    static printTask(list) {
         let todoCont = document.querySelector("#todoCont");
         todoCont.innerHTML = '';
         list.forEach((task) => {
@@ -37,12 +92,5 @@ class UI {
           todoCont.appendChild(taskCard);
         });
       }
-}
-
-export default UI;
-
-
-
-
-
+*/
 
--- src/index.js
+++ src/index.js
@@ -10,33 +10,25 @@ const mainDiv = document.querySelector("#content");
 mainDiv.classList.add('d-flex');
 mainDiv.innerHTML = `
 <div id="projects" class="w-25"></div>
-
-<form action="/" class="d-flex flex-column w-25 mx-auto d-none" id="todoForm">
-<label for="title" class="form-label">Title</label>
-<input type="text" id="title" class="form-control" required>
-
-<label for="description" class="form-label">Description</label>
-<input type="text" id="description" class="form-control" required>
-
-<label for="dueDate" class="form-label">Due Date</label>
-<input type="date" id="dueDate" class="form-control" required>
-
-<label for="priority" class="form-label">Priority</label>
-<select id="priority" name="priority" class="form-control" required>
-  <option value="low">low</option>
-  <option value="mid">Mid</option>
-  <option value="high">High</option>
-</select>
-
-<input type="submit" id="submit-btn" value="Create a new To Do" class="btn btn-primary">
-</form> 
-
-
-
-
-<div id="todoCont" class="w-75"></div>
-
+<div id="todos"class="w-75"> 
+  <div id="todoCont" class="">
+    <table class="table" id="todoTable">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Due Date</th>
+        </tr>
+      </thead>
+      <tbody id="todoBody">
+      </tbody>
+    </table>  
+  </div>
+  <button id="btnForm" class="btn btn-primary">Add +</button>
+</div>
 `;
+
+UI.printForm();
 
 if (localStorage.getItem('myTasksStorage') === null) {
     todoArr = [];
@@ -52,9 +44,15 @@ document.getElementById("todoForm").addEventListener("submit", UI.addTodo);
 
 document.getElementById('todoCont').addEventListener('click', (e) => {
   if (e.target.classList.contains('delete-btn')) {
-    Store.removeTask(e.target.parentElement.firstChild.nextElementSibling.textContent, todoArr);
+    //Store.removeTask(e.target.parentElement.firstChild.nextElementSibling.textContent, todoArr);
+    console.log(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+    Store.removeTask(e.target.parentElement.previousElementSibling.previousElementSibling.textContent, todoArr);
     e.target.parentElement.parentElement.remove(); 
  }
+});
+
+document.getElementById("btnForm").addEventListener('click', () => {
+  document.getElementById("formCont").classList.toggle('none');
 });
 
 export default todoArr;
