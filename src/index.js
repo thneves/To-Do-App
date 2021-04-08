5da6aea4b014287d7e8e03e65c45afb7766d89bc
AlanSoto31 <soto.alan@hotmail.com>
2021-04-08 14:19:21 -0500

    Add projects logic

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./doom";
 import UI from "./doom";
+import Project from "./projects";
 import Store from './storage';
 
-let todoArr = [];
+let todoArr = ["hola", "hello"];
+
+let defaultProject = new Project("Default Project", todoArr);
+
+let projectArr = [defaultProject];
 
 const mainDiv = document.querySelector("#content");
 mainDiv.classList.add('d-flex');
 mainDiv.innerHTML = `
-<div id="projects" class="w-25"></div>
+<div id="projects" class="w-25">
+<button id="addProject" class="btn btn-primary">+Add</button>
+<ul id="projectLits"></ul>
+</div>
 <div id="todos"class="w-75"> 
   <div id="todoCont" class="">
     <table class="table" id="todoTable">
@@ -26,9 +34,50 @@ mainDiv.innerHTML = `
     </table>  
   </div>
   <button id="btnForm" class="btn btn-primary">Add +</button>
-  <button id="printArr" class="btn btn-primary">Print Array</button>
 </div>
 `;
+
+
+
+
+
+let projects = document.getElementById("projects");
+
+let addProject = () => {
+  let newProject = new Project("Project1", [])
+  projectArr.push(newProject);
+  printProjects();
+}
+
+let printProjects = () => {
+  let projectList = document.getElementById("projectLits");
+  projectList.innerHTML = '';
+  projectArr.forEach( (project, index) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
+    <a href="#" id="project${index}" class="projectLinks">${project.name}</a>
+    `;
+    projectList.appendChild(li);
+  })
+}
+
+document.getElementById('projectLits').addEventListener('click', (e) => {
+  //console.log(e.target.textContent);
+  if (e.target.classList.contains('projectLinks')) {
+    projectArr.forEach(project => {
+      if(project.name === e.target.textContent) {
+        console.log(project.todoList);
+        UI.printTask(project.todoList);
+      }
+    })
+  } 
+});
+
+document.getElementById("addProject").addEventListener("click", addProject);
+
+
+
+
 
 
 if (localStorage.getItem('myTasksStorage') === null) {
@@ -64,10 +113,6 @@ document.getElementById('todoCont').addEventListener('click', (e) => {
 
 document.getElementById("btnForm").addEventListener('click', () => {
   document.getElementById("formCont").classList.toggle('none');
-});
-
-document.getElementById("printArr").addEventListener('click', () => {
-  console.log(todoArr);
 });
 
 export default todoArr;
---
+++ src/projects.js
@@ -0,0 +1,8 @@
+class Project {
+    constructor(name, todoList){
+        this.name = name;
+        this.todoList = todoList;
+    }
+}
+
+export default Project;
